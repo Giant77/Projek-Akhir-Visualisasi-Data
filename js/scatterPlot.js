569f835a26b41f21e9e59d56e9cf345d649fefd1
Giant77 <willyarsyad77@gmail.com>
2024-05-04 17:47:45 +0700

    feat: add attribute dropdown selection

--- js/scatterPlot.js
+++ js/scatterPlot.js
@@ -13,6 +13,24 @@ const svg = d3
 
 //Read the data
 d3.csv("./data/nutrition.csv").then((data) => {
+    const allGroup = [
+      "Proteins X Calories",
+      "Fat X Calories",
+      "Carbohydrate X Calories",
+      "Fat X Proteins",
+      "Carbohydrate X Proteins",
+      "Carbohydrate X Fat",
+    ];
+
+    // add the options to the button
+    d3.select("#select-scatter-plot")
+        .selectAll("myOptions")
+        .data(allGroup)
+        .enter()
+        .append("option")
+        .text((d) => d) // text showed in the menu
+        .attr("value", (d) => d.toLowerCase()); // corresponding value returned by the button
+
     // Add X axis
     const x = d3.scaleLinear().domain([0, 0]).range([0, width]);
     svg.append("g")
@@ -26,7 +44,8 @@ d3.csv("./data/nutrition.csv").then((data) => {
     svg.append("g").call(d3.axisLeft(y));
 
     // Add dots
-    svg.append("g")
+    const dot = svg
+        .append("g")
         .selectAll("dot")
         .data(data)
         .enter()
@@ -63,7 +82,8 @@ d3.csv("./data/nutrition.csv").then((data) => {
         });
 
     // Add X axis label
-    svg.append("text")
+    var xLabel = svg
+        .append("text")
         .attr("class", "x-axis-label")
         .attr("text-anchor", "middle")
         .attr("x", width / 2)
@@ -71,11 +91,51 @@ d3.csv("./data/nutrition.csv").then((data) => {
         .text("Calories");
 
     // Add Y axis label
-    svg.append("text")
+    var yLabel = svg
+        .append("text")
         .attr("class", "y-axis-label")
         .attr("text-anchor", "middle")
         .attr("transform", "rotate(-90)")
         .attr("x", -height / 2)
         .attr("y", -margin.left + 28) // Adjust this value for positioning
         .text("Proteins");
+
+    function update(selectedOption) {
+        const option = selectedOption.split(" x ");
+        console.log(option);
+
+        // Create new data with the selection
+        const dataFilter = data.map(function (d) {
+            return { attrX: d[option[1]], attrY: d[option[0]] };
+        });
+
+        // console.log(dataFilter);
+
+        dot.data(dataFilter)
+            .transition()
+            .duration(1000)
+            .attr("cx", (d) => x(+d.attrX))
+            .attr("cy", (d) => y(+d.attrY));
+
+        // update label on x and y axis
+        yLabel.text(option[0]);
+        xLabel.text(option[1]);
+
+        // if (option[0] === "fat" && option[1] === "proteins") {
+        //   y.domain(newDomain);
+        //   // Update y-axis
+        //   svg.select(".y-axis")
+        //       .transition()
+        //       .duration(1000)
+        //       .call(d3.axisLeft(y));
+        // }
+    }
+
+    // When the button is changed, run the updateChart function
+    d3.select("#select-scatter-plot").on("change", function (event, d) {
+        // recover the option that has been chosen
+        let selectedOption = d3.select(this).property("value");
+        // run the updateChart function with this selected option
+        update(selectedOption);
+    });
 });
